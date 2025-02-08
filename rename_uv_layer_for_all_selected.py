e09f3b468414a6701cde09cdfee79537d53c6865
Christopher Ferris <christopher.d.ferris@gmail.com>
2025-02-08 11:51:36 -0800

    added rename_uv_layer_for_all_selected

---
+++ rename_uv_layer_for_all_selected.py
@@ -0,0 +1,17 @@
+import bpy
+
+def rename_uv_layer_for_all_selected(current_name, new_name):
+    """
+    Renames all UV Layers with the name current_name to new_name
+    Especially helpful when working with multiple UV layers across multiple objects
+
+    Paramteters:
+    current_name (string): the current name of the UV layer to rename
+    new_name (string): the new name of the UV layer
+    """
+    for obj in bpy.context.selected_objects:
+        if obj.data != None:
+            if hasattr(obj.data, "uv_layers"):
+                for uv_layer in obj.data.uv_layers:
+                    if uv_layer.name == current_name:
+                        uv_layer.name = new_name
