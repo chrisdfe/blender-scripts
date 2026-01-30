5695e78b0bcd28f984ae5cbae86aa2da9e81a92b
Christopher Ferris <christopher.d.ferris@gmail.com>
2026-01-29 18:29:45 -0800

    comments

--- addons/sweaterparrot_functions/mark_sharp_as_seam_and_unwrap.py
+++ addons/sweaterparrot_functions/mark_sharp_as_seam_and_unwrap.py
@@ -18,20 +18,14 @@ class SeamsFromSharp(bpy.types.Operator):
     mesh = context.edit_object.data
     bm = bmesh.from_edit_mesh(mesh)
 
-    # CRITICAL: Update the selection indices so the script "sees" your selection
     bm.edges.ensure_lookup_table()
 
     # In Blender 4.x/5.0, this is the standard way to check sharpness in BMesh
     # sharp_layer = bm.edges.layers.bool.get("sharp_edge")
 
-    # if sharp_layer is None:
-    #     self.report({'WARNING'}, "No sharp edges found in the mesh data.")
-    #     return {'CANCELLED'} 
-
-      # Set sharp edges as seam edges also
+    # Set sharp edges as seam edges also
     count = 0
     for edge in bm.edges:
-        # if edge.select and edge[sharp_layer]:
         if edge.select and not edge.smooth:
             edge.seam = True
             count += 1
@@ -47,6 +41,7 @@ def add_to_edge_menu(self, context):
 
 def register():
     bpy.utils.register_class(SeamsFromSharp)
+
     # VIEW3D_MT_edit_mesh_edges is the ID for the Ctrl + E menu
     bpy.types.VIEW3D_MT_edit_mesh_edges.append(add_to_edge_menu)
 
